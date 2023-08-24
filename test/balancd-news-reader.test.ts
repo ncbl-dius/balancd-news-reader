1632476a6e5dd81302cb97b256637798fd0012b2
Nick Laycock <nlaycock@dius.com.au>
2023-08-24 12:45:35 +1000

    initial commit

---
+++ test/balancd-news-reader.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BalancdNewsReader from '../lib/balancd-news-reader-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/balancd-news-reader-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BalancdNewsReader.BalancdNewsReaderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
